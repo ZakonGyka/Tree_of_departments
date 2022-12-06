9ab3264a1b5ac64c7b7d14c98629135bf5ef3299
zakongyka <zakongyka@yandex.ru>
2022-12-06 02:16:25 +0100

    add employees/admin

--- tree_structure/employees/admin.py
+++ tree_structure/employees/admin.py
@@ -3,4 +3,25 @@ from employees.models import Worker
 
 # from . import random_employee
 
-admin.site.register(Worker)
+
+@admin.register(Worker)
+class EmployeesAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'full_name',
+        'position',
+        'employment_date',
+        'salary',
+        'parent',
+        'date_added'
+    )
+    list_filter = ('position',)
+    list_display_links = ('full_name',)
+    search_fields = ('full_name',
+                     'employment_date',
+                     'salary',
+                     )
+    empty_value_display = '-отсутствует-'
+
+
+# admin.site.register(Worker, EmployeesAdmin)
